0cb6e9cf2117795a6c9c4660e31472d82aae01b9
Stashkov, Vladimir <testct2@gmail.com>
2018-04-13 20:02:30 +0200

    reformat

--- src/main/java/graphics/Texture.java
+++ src/main/java/graphics/Texture.java
@@ -13,15 +13,17 @@ public class Texture
     private int width, height;
     private int texture;
 
-    public Texture(String path){
-        texture = load(path);
+    public Texture( String path )
+    {
+        texture = load( path );
     }
 
     private int load( String path )
 
     {
         int[] pixels = null;
-        try{
+        try
+        {
             BufferedImage image = ImageIO.read( new FileInputStream( path ) );
             width = image.getWidth();
             height = image.getHeight();
@@ -34,31 +36,36 @@ public class Texture
         }
 
         int[] data = new int[width * height];
-        for (int i = 0; i < width; i++)
+        assert pixels != null;
+        for ( int i = 0; i < width; i++ )
         {
-            int a = (pixels[i] & 0xff000000) >> 24;
-            int r = (pixels[i] & 0xff0000) >> 16;
-            int g = (pixels[i] & 0xff00) >> 8;
-            int b = (pixels[i] & 0xff);
+            int a = ( pixels[i] & 0xff000000 ) >> 24;
+            int r = ( pixels[i] & 0xff0000 ) >> 16;
+            int g = ( pixels[i] & 0xff00 ) >> 8;
+            int b = ( pixels[i] & 0xff );
 
             data[i] = a << 24 | b << 16 | g << 8 | r;
         }
 
-        int result  = glGenTextures();
+        int result = glGenTextures();
         glBindTexture( GL_TEXTURE_2D, result ); //select a layer
 
 
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );  // upscale
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, createIntBuffer(data) );
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, createIntBuffer( data ) );
         glBindTexture( GL_TEXTURE_2D, 0 ); //deselect everything
         return result;
     }
-    public void bind(){
+
+    public void bind()
+    {
         glBindTexture( GL_TEXTURE_2D, texture ); //select a layer
 
     }
-    public void unbind(){
+
+    public void unbind()
+    {
         glBindTexture( GL_TEXTURE_2D, 0 ); //deselect everything
 
     }
