d978614b34bc07ab2fdd9dd9e458bea0f12ca6a6
Stashkov, Vladimir <testct2@gmail.com>
2018-04-14 19:28:24 +0200

    reformat

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -4,24 +4,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileUtils {
+class FileUtils
+{
 
-	private FileUtils() {
-	}
-	
-	public static String loadAsString(String file) {
-		StringBuilder result = new StringBuilder();
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String buffer;
-			while ((buffer = reader.readLine()) != null) {
-				result.append( buffer ).append( '\n' );
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result.toString();
-	}
+    private FileUtils()
+    {
+    }
+
+    static String loadAsString( String file )
+    {
+        StringBuilder result = new StringBuilder();
+        try
+        {
+            BufferedReader reader = new BufferedReader( new FileReader( file ) );
+            String buffer;
+            while ( ( buffer = reader.readLine() ) != null )
+            {
+                result.append( buffer ).append( '\n' );
+            }
+            reader.close();
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
 
 }
