ca6cf54b7bca064d81dee383f71429132e6eb816
Stashkov, Vladimir <testct2@gmail.com>
2018-04-14 20:03:06 +0200

    qualify class variables explicitly

--- src/main/java/maths/Vector3f.java
+++ src/main/java/maths/Vector3f.java
@@ -2,20 +2,22 @@ package maths;
 
 public class Vector3f
 {
+
     public float x, y, z;
+
 
     public Vector3f()
     {
         x = 0.0f;
         y = 0.0f;
         z = 0.0f;
-
     }
 
     public Vector3f( float x, float y, float z )
     {
-        x = x;
-        y = y;
-        z = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
+
 }
